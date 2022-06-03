f5f50b2f62dab235ac3a329a160711c842f3ae29
Christopher Analikwu <analikwuchristopher@gmail.com>
2022-06-03 02:22:41 +0200

    setting up the routes and redux actions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header";
+import AddExpense from "./components/pages/addexpense";
 import Home from "./components/pages/home";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Header />
-      </div>
-      <Home />
+    <Router>
+      <Header />
+      <Switch>
+        {/* Anything you want to change with path you simply put it inside this switch statement */}
+        <Route path="/" exact component={Home} />
+        <Route path="/addexpense" component={AddExpense} />
+      </Switch>
+
       <div>Footer</div>
-    </div>
+    </Router>
   );
 }
 
